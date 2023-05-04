a45d20c783fdde5cb7c40f0b8fb6346bdbedfc99
Documenter.jl <documenter@juliadocs.github.io>
2023-05-04 14:02:23 +0000

    build based on 2f6ee86

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PolyaGammaHybridSamplers","category":"page"},{"location":"#PolyaGammaHybridSamplers","page":"Home","title":"PolyaGammaHybridSamplers","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PolyaGammaHybridSamplers.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PolyaGammaHybridSamplers]","category":"page"}]
+}
