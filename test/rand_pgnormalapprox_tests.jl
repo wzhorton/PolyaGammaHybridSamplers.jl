e55b18d855ad9d2685e9e77f3bb81cb1873d75c6
Zach Horton <wz.horton@gmail.com>
2023-05-06 13:18:16 -0700

    remove copied comments

--- test/rand_pgnormalapprox_tests.jl
+++ test/rand_pgnormalapprox_tests.jl
@@ -7,9 +7,6 @@ function test_rand_b0_normalapprox()
     @test PolyaGammaHybridSamplers.rand_pgnormalapprox(0, 1.0, Random.GLOBAL_RNG) ≈ 0
 end
 
-# No tests on Base.rand as it is just a wrapper for the rand_ functions
-# No test on rand_pghybrid as it is just a wrapper for the other rand_ functions
-
 # Run tests
 @testset "Sampling method tests" begin
     test_rand_b0_normalapprox()
