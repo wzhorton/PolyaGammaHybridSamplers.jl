c0d5b3dd4a4f4b91ab6f01878642f2abbc0b9ed7
Zach Horton <wz.horton@gmail.com>
2023-05-06 19:11:13 -0700

    rename pghybrid test file

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using SpecialFunctions
 using PolyaGammaHybridSamplers
 import PolyaGammaHybridSamplers: PGSamplingMethod
 
-include("PolyaGammaHybridSamplers_test.jl")
+include("pghybrid_tests.jl")
 include("rand_pgnormalapprox_tests.jl")
 include("rand_pgdevroye_tests.jl")
 include("rand_pgsaddlepoint_tests.jl")
