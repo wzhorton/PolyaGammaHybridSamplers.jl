13625f45684a1ad0ad43b84287da9a1b5b81e119
Zach Horton <wz.horton@gmail.com>
2023-05-06 18:43:28 -0700

    comment corrections

--- test/rand_pgdevroye_tests.jl
+++ test/rand_pgdevroye_tests.jl
@@ -2,12 +2,12 @@
 # Sampling method tests
 #----------------------------------#
 
-# Test that the rand_pgnormalapprox function return 0 for b = 0
+# Test that the rand_pgdevroye function return 0 for b = 0
 function test_rand_b0_devroye()
     @test PolyaGammaHybridSamplers.rand_pgdevroye(0, 1.0, Random.GLOBAL_RNG) ≈ 0
 end
 
-# Test that a simulation of 10,000 draws from a PG(2, 1) distribution has the correct mean and variance
+# Test that a simulation of 100,000 draws from a PG(2, 1) distribution has the correct mean and variance
 function test_rand_pgdevroye_mean_var()
     b = 2
     z = 1.0
